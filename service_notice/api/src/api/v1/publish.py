48c8856f016dfee9ce51e8b2824c45a341b9bb3d
lukin_ma <@>
2023-03-08 20:45:24 +0400

    add QUEUE_NAME

--- service_notice/api/src/api/v1/publish.py
+++ service_notice/api/src/api/v1/publish.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException, status
 
 from src.config import producer
 from src.schemas import Message
+from src.settings import settings
 
 router = APIRouter()
 
@@ -9,9 +10,9 @@ router = APIRouter()
 @router.post("/publish/")
 async def publish(message: Message):
     try:
-        return await producer.publish(queue_name="email", message=message.dict())
+        return await producer.publish(queue_name=settings.QUEUE_NAME, message=message.dict())
     except RuntimeError:
         await producer.connect_broker()
-        return await producer.publish(queue_name="email", message=message.dict())
+        return await producer.publish(queue_name=settings.QUEUE_NAME, message=message.dict())
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
