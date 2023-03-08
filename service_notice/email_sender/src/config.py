929a97155d50af53f4fb90fdf6724f3015d8482e
Sergey Morichev <s.morichev@yandex.ru>
2023-03-08 17:28:16 +0500

    fix config

--- service_notice/email_sender/src/config.py
+++ service_notice/email_sender/src/config.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings, Field
 
 
 class Settings(BaseSettings):
-    SEND_FROM_EMAIL: str = Field("test@example.com", env="SENDGRID_FROM_EMAIL")
+    SEND_FROM_EMAIL: str = Field("test@example.com", env="EMAIL_SENDER_FROM_EMAIL")
     DEBUG: bool = Field(True, env="EMAIL_SENDER_DEBUG")
     RABBITMQ_HOST: str = Field("localhost", env="RABBITMQ_NOTICE_HOST")
     RABBITMQ_PORT: int = Field(5672, env="RABBITMQ_NOTICE_PORT")
