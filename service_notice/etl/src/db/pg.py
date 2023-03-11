1e55c1cd70387d4d4b4bf2df790339a92de87ccf
RomanBorovskiy <59736627+RomanBorovskiy@users.noreply.github.com>
2023-03-11 19:18:09 +0300

    add template db init

--- service_notice/etl/src/db/pg.py
+++ service_notice/etl/src/db/pg.py
@@ -4,11 +4,24 @@ import psycopg2
 from psycopg2.extensions import connection as PGConnection
 from psycopg2.extras import DictCursor
 
+INIT_QUERY = """
+CREATE TABLE IF NOT EXISTS template (
+    id VARCHAR(255) PRIMARY KEY,
+    template_text TEXT NOT NULL);
+INSERT INTO template (id, template_text) VALUES('default','Привет, {{username}}. Ты получаешь {{present}}!')
+ON CONFLICT (id) DO UPDATE
+    SET template_text = EXCLUDED.template_text;
+"""
+
 
 class PostgresDB:
     def __init__(self, uri: str):
         logging.debug("start PostgreSQL")
         self.connection: PGConnection = psycopg2.connect(uri, cursor_factory=DictCursor)
+        with self.connection.cursor() as cursor:
+            cursor.execute(INIT_QUERY)
+            logging.debug('init tables')
+        self.connection.commit()
 
     def execute_query(self, query: str) -> list[tuple]:
         with self.connection.cursor() as cursor:
