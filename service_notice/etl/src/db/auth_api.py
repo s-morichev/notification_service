31cd3c236f5e5934628ff461eddc8320063a67cf
RomanBorovskiy <59736627+RomanBorovskiy@users.noreply.github.com>
2023-03-12 16:56:28 +0300

    add function for many users

--- service_notice/etl/src/db/auth_api.py
+++ service_notice/etl/src/db/auth_api.py
@@ -32,3 +32,27 @@ def get_user_info_from_auth(request_id: str, user_id: UUID) -> UserInfo | None:
                     return UserInfo(**users_info[0])
 
     return None
+
+
+def get_users_info_from_auth(request_id: str, user_ids: list[UUID]) -> dict[UUID, UserInfo] | None:
+    auth_srv = settings.AUTH_SERVICE_URI
+    path = "/auth/v1/userinfo"
+    url = f"http://{auth_srv}{path}"
+    data = orjson.dumps({"user_ids": user_ids})
+    headers = {"Authorization": settings.SECRET_KEY, "X-Request-Id": request_id, "Content-Type": "application/json"}
+
+    try:
+        response = requests.post(url, headers=headers, data=data)
+
+    except RequestException as err:
+        logging.error(f'error call auth service. Error: {err}')
+
+    else:
+        if response.status_code == HTTPStatus.OK:
+            data = response.json()
+            if data and "users_info" in data:
+                users_info = data["users_info"]
+                result = {item['user_id']: UserInfo(**item) for item in users_info}
+                return result
+
+    return None
