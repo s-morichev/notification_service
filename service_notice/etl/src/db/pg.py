c9400648745056e5ea405f179b5381bf28748409
RomanBorovskiy <59736627+RomanBorovskiy@users.noreply.github.com>
2023-03-17 19:12:10 +0300

    fix pg query

--- service_notice/etl/src/db/pg.py
+++ service_notice/etl/src/db/pg.py
@@ -5,13 +5,9 @@ from psycopg2.extensions import connection as PGConnection
 from psycopg2.extras import DictCursor
 import backoff
 
-INIT_QUERY = """
-CREATE TABLE IF NOT EXISTS template (
-    id VARCHAR(255) PRIMARY KEY,
-    template_text TEXT NOT NULL);
-INSERT INTO template (id, template_text) VALUES('default','Привет, {{username}}. Ты получаешь {{present}}!')
-ON CONFLICT (id) DO UPDATE
-    SET template_text = EXCLUDED.template_text;
+CHECK_QUERY = """
+SELECT id, subject, body
+FROM public.templates limit 1;
 """
 
 
@@ -21,9 +17,9 @@ class PostgresDB:
         logging.debug("start PostgreSQL")
         self.connection: PGConnection = psycopg2.connect(uri, cursor_factory=DictCursor)
         with self.connection.cursor() as cursor:
-            cursor.execute(INIT_QUERY)
-            logging.debug("init tables")
-        self.connection.commit()
+            cursor.execute(CHECK_QUERY)
+            cursor.fetchall()
+            logging.debug("check table templates")
 
     def execute_query(self, query: str) -> list[tuple]:
         with self.connection.cursor() as cursor:
@@ -40,10 +36,10 @@ class PostgresDB:
 db: PostgresDB | None = None
 
 
-def get_template_from_db(template_id: str) -> str:
-    query = "select template_text from template where id='{0}'"
+def get_template_from_db(template_id: str) -> tuple[str, str]:
+    query = "select subject, body from public.templates where id='{0}'"
     result = db.execute_query(query.format(template_id))
     if result:
-        return result[0][0]
+        return result[0]
     else:
-        return ""
+        return '', ''
