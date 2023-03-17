6a98902294f7ace9d48423299eb6201fd8a3a47c
Sergey Morichev <s.morichev@yandex.ru>
2023-03-17 18:31:13 +0500

    fix host names

--- service_notice/api/src/main.py
+++ service_notice/api/src/main.py
@@ -6,4 +6,4 @@ from src.config import app
 app.include_router(publish.router, prefix="/api/v1", tags=["Publish"])
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
